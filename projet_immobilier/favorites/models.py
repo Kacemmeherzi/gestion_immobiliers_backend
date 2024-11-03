9d094f66e3b22c822239be429585da621cc88623
kacem <kacemmeherzi52@gmail.com>
2024-11-03 18:29:20 +0100

    favorite model added

--- projet_immobilier/favorites/models.py
+++ projet_immobilier/favorites/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from app_annonces.models import Annonce
 
 # Create your models here.
+class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    annonce = models.ForeignKey(Annonce, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'annonce')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.annonce.titre}"
