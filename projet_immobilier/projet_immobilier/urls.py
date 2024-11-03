904b9127128102639b8600f8e758a3a77543e965
kacem <kacemmeherzi52@gmail.com>
2024-11-03 23:16:16 +0100

    favorite added to the main urls

--- projet_immobilier/projet_immobilier/urls.py
+++ projet_immobilier/projet_immobilier/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('app_annonces.urls')),
+    path('favorites/', include('favorites.urls')),   
+
     
 ]
