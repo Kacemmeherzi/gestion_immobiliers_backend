2e4f5771255f3709675aa124fac94e73dd96c033
kacem <kacemmeherzi52@gmail.com>
2024-11-03 23:15:44 +0100

    crud urls added

--- projet_immobilier/favorites/urls.py
+++ projet_immobilier/favorites/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import AnnonceListCreate, AnnonceDetail
+from .views import AddFavoriteView, FavoriteDetailView, FavoriteListView
 
 urlpatterns = [
-    
+    path('add/', AddFavoriteView.as_view(), name='add_favorite'),  # Create
+    path('getall/<int:id>/', FavoriteListView.as_view(), name='favorite_list'),   # List
+    path('findone/<int:id>/', FavoriteDetailView.as_view(), name='favorite_detail'), 
 ]
