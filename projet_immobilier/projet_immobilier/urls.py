7540c45480d33406eb14a4621499eb352b57ada5
kacem <kacemmeherzi52@gmail.com>
2024-11-04 20:23:03 +0100

    url added to the main urls file

--- projet_immobilier/projet_immobilier/urls.py
+++ projet_immobilier/projet_immobilier/urls.py
@@ -21,6 +21,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('annonce/', include('app_annonces.urls')),
     path('favorite/', include('favorites.urls')),   
+    path('commentaires/', include('commentaires.urls')),   
+
 
     
 ]
