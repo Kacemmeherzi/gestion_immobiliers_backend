ff2635f9562f0d62eb64c39efc0722ed8111a523
kacem <kacemmeherzi52@gmail.com>
2024-11-04 10:56:17 +0100

    favorite serializer fixed to take user id and annonce id

--- projet_immobilier/favorites/serializers.py
+++ projet_immobilier/favorites/serializers.py
@@ -2,15 +2,24 @@
 from rest_framework import serializers
 
 from app_annonces.serializers import AnnonceSerializer
+from app_annonces.models import Annonce
 from .models import Favorite
 
 class FavoriteSerializer(serializers.ModelSerializer):
-    annonce = serializers.SerializerMethodField()
-
+    annonce = serializers.PrimaryKeyRelatedField(queryset=Annonce.objects.all())  # Expect an ID for creation
     class Meta:
         model = Favorite
         fields = ['id', 'user', 'annonce', 'created_at']
-    def get_annonce(self, obj):
-        # You can add logic here to return just the annonce details or a link to expand
-        return AnnonceSerializer(obj.annonce).data
+
+    def create(self, validated_data):
+
+        favorite = Favorite.objects.create(**validated_data)
+        return favorite
+
+    def to_representation(self, instance):
+        # Call the original representation method
+        representation = super().to_representation(instance)
+        # Modify the annonce field to use the AnnonceSerializer
+        representation['annonce'] = AnnonceSerializer(instance.annonce).data
+        return representation
        
