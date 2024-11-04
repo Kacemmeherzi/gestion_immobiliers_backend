bbaf7b19c391d9f6137aeda32f0939b48493f0b4
kacem <kacemmeherzi52@gmail.com>
2024-11-04 10:55:26 +0100

    url improved

--- projet_immobilier/app_annonces/urls.py
+++ projet_immobilier/app_annonces/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import AnnonceListCreate, AnnonceDetail
 
 urlpatterns = [
-    path('annonces/', AnnonceListCreate.as_view(), name='annonce-list-create'),
+    path('', AnnonceListCreate.as_view(), name='annonce-list-create'),
     path('annonces/<int:pk>/', AnnonceDetail.as_view(), name='annonce-detail'),
 ]
