2978792d137d0d04c815b423407c61d19a2dc9b2
kacem <kacemmeherzi52@gmail.com>
2024-11-16 22:59:37 +0100

    occupation model added

--- projet_immobilier/occupations/models.py
+++ projet_immobilier/occupations/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+from app_annonces.models import Annonce
+
 # Create your models here.
+class Occupation(models.Model):
+  #  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='occupations')
+  #  client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='occupations')
+    occupation_type = models.CharField(max_length=20)
+    annonce = models.ForeignKey(Annonce, on_delete=models.CASCADE, related_name='occupations')
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.occupation_type.name}"
