8a637408f6235744865fcf6bce37223fc43dbacd
kacem <kacemmeherzi52@gmail.com>
2024-11-18 10:11:08 +0100

    occupation serializer added

---
+++ projet_immobilier/occupations/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+
+from app_annonces.serializers import AnnonceSerializer
+from users.models import UserProfile
+from .models import Occupation
+from django.contrib.auth.models import User
+
+
+class UserProfileserializer(serializers.ModelSerializer) : 
+    class Meta:
+        model = UserProfile
+        fields = ['phone_number', 'role']
+# Nested serializer for User
+class UserSerializer(serializers.ModelSerializer):
+    profile = UserProfileserializer()
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name','profile']
+
+
+# Occupation Serializer with nested User and Annonce
+class OccupationSerializer(serializers.ModelSerializer):
+    owner = UserSerializer()  # Nested User serializer for owner
+    client = UserSerializer()  # Nested User serializer for client
+    annonce = AnnonceSerializer()  # Nested Annonce serializer for annonce
+
+    class Meta:
+        model = Occupation
+        fields = ['id', 'owner', 'client', 'occupation_type', 'annonce', 'start_date', 'end_date', 'is_active']
