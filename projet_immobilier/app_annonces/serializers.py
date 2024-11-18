9dc7dbb70c6a740ac4bcc9e448521381501f8acf
kacem <kacemmeherzi52@gmail.com>
2024-11-18 21:31:40 +0100

    AnnonceCreateSerializer added

--- projet_immobilier/app_annonces/serializers.py
+++ projet_immobilier/app_annonces/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
+
+from occupations.models import Occupation
 from .models import Annonce
 
 class AnnonceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Annonce
         fields = '__all__'
+
+
+
+class AnnonceCreateSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+   
+    class Meta:
+        model = Occupation
+        fields = '__all__'
+    
