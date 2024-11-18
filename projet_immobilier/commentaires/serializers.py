2a546a503e3f0e09989947ee4dd531bf5f923925
kacem <kacemmeherzi52@gmail.com>
2024-11-18 22:42:17 +0100

    create serializer added to take ids instead of the hole object

--- projet_immobilier/commentaires/serializers.py
+++ projet_immobilier/commentaires/serializers.py
@@ -1,4 +1,7 @@
 from rest_framework import serializers
+
+from app_annonces.models import Annonce
+from django.contrib.auth.models import User
 from .models import Commentaire
 
 class CommentaireSerializer(serializers.ModelSerializer):
@@ -6,3 +9,26 @@ class CommentaireSerializer(serializers.ModelSerializer):
         model = Commentaire
         fields = ['id', 'annonce', 'user', 'contenu', 'date_creation']
         read_only_fields = ['date_creation']
+
+        
+class CommentaireCreateSerializer(serializers.ModelSerializer):
+    user_id = serializers.IntegerField(write_only=True)  # Takes the user_id in the request
+    annonce_id = serializers.IntegerField(write_only=True)  # Takes the annonce_id in the request
+
+    class Meta:
+        model = Commentaire
+        fields = ['user_id', 'annonce_id', 'contenu', 'date_creation']
+
+    def create(self, validated_data):
+        # Get the user and annonce objects using the user_id and annonce_id
+        user = User.objects.get(id=validated_data['user_id'])
+        annonce = Annonce.objects.get(id=validated_data['annonce_id'])
+
+        # Create a new Commentaire instance with the retrieved user and annonce
+        commentaire = Commentaire.objects.create(
+            user=user,
+            annonce=annonce,
+            contenu=validated_data['contenu']
+        )
+
+        return commentaire
