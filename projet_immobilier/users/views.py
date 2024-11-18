654a068dfa804a582c741d69281348b365793739
kacem <kacemmeherzi52@gmail.com>
2024-11-18 10:18:05 +0100

    login and registraion added

--- projet_immobilier/users/views.py
+++ projet_immobilier/users/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import LoginSerializer
+from rest_framework import status
+from .serializers import RegistrationSerializer
 
-# Create your views here.
+class RegisterAPIView(APIView):
+    def post(self, request):
+        serializer = RegistrationSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()  # This will create the user and the user profile
+            
+            return Response({
+                'message': 'Registration successful.',
+                'user': {
+                    'id': user.id,
+                    'username': user.username,
+                    'email': user.email,
+                    'first_name': user.first_name,
+                    'last_name': user.last_name
+                },
+                'profile': {
+                    'phone_number': user.profile.phone_number,
+                    'role': user.profile.role
+                }
+            }, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            
+            
+            return Response({
+                'message': 'Login successful.',
+                'user': {
+                    'id': user.id,
+                    'username': user.username,
+                    'email': user.email,
+                    'first_name': user.first_name,
+                    'last_name': user.last_name,
+                    'phone_number': user.profile.phone_number,
+                    'role': user.profile.role
+                }
+            }, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
