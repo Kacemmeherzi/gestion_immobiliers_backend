3fbd3daa167cf1e5a5b6821ed49cdc023665bf6a
kacem <kacemmeherzi52@gmail.com>
2024-11-18 10:13:01 +0100

    occupations urls added

---
+++ projet_immobilier/occupations/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import get_all_occupations, get_occupations_by_client, get_occupations_by_owner
+
+urlpatterns = [
+    path('client/<int:user_id>/', get_occupations_by_client, name='get_occupations_by_client'),
+    path('owner/<int:user_id>/', get_occupations_by_owner, name='get_occupations_by_owner'),
+    path('all/', get_all_occupations, name='get_all_occupations'),
+
+]
