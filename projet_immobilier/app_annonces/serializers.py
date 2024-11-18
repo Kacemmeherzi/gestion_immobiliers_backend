6d1f87798bdcb9df0b48f5ac97dc43599215e360
kacem <kacemmeherzi52@gmail.com>
2024-11-18 22:55:42 +0100

    user and user info included in the annonce

--- projet_immobilier/app_annonces/serializers.py
+++ projet_immobilier/app_annonces/serializers.py
@@ -1,10 +1,19 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-
-from occupations.models import Occupation
+from users.models import UserProfile
 from .models import Annonce
-
+class UserProfileserializer(serializers.ModelSerializer) : 
+    class Meta:
+        model = UserProfile
+        fields = ['phone_number', 'role']
+# Nested serializer for User
+class UserSerializer(serializers.ModelSerializer):
+    profile = UserProfileserializer()
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name','profile']
 class AnnonceSerializer(serializers.ModelSerializer):
+    owner = UserSerializer()
     class Meta:
         model = Annonce
         fields = '__all__'
@@ -12,9 +21,9 @@ class AnnonceSerializer(serializers.ModelSerializer):
 
 
 class AnnonceCreateSerializer(serializers.ModelSerializer):
-    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    owner = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
    
     class Meta:
-        model = Occupation
+        model = Annonce
         fields = '__all__'
     
