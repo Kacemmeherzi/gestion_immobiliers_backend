64ac8c56c87fa55f8ebe880166e77bcdd29d06c3
kacem <kacemmeherzi52@gmail.com>
2024-11-18 21:33:35 +0100

    create method added

--- projet_immobilier/occupations/urls.py
+++ projet_immobilier/occupations/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import get_all_occupations, get_occupations_by_client, get_occupations_by_owner
+from .views import create_occupation, get_all_occupations, get_occupations_by_client, get_occupations_by_owner
 
 urlpatterns = [
     path('client/<int:user_id>/', get_occupations_by_client, name='get_occupations_by_client'),
     path('owner/<int:user_id>/', get_occupations_by_owner, name='get_occupations_by_owner'),
     path('all/', get_all_occupations, name='get_all_occupations'),
+    path('create',create_occupation , name='get_all_occupations'),
 
 ]
