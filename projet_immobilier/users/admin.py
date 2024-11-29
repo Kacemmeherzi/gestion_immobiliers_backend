8500685c85191649c62ffafad74706bc47d5e077
kacem <kacemmeherzi52@gmail.com>
2024-11-29 21:48:32 +0100

    fixed

--- projet_immobilier/users/admin.py
+++ projet_immobilier/users/admin.py
@@ -23,7 +23,7 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'email', 'first_name', 'last_name', 'password1', 'password2'),
+            'fields': ('username', 'email', 'first_name', 'last_name', 'password1', 'password2',),
         }),
     )
 
