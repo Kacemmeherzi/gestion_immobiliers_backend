494731d5e25d401190235b5dcc2a0dd25a2587c4
kacem <kacemmeherzi52@gmail.com>
2024-11-29 21:46:10 +0100

    improved add offered price attribut code fixed

--- projet_immobilier/occupations/models.py
+++ projet_immobilier/occupations/models.py
@@ -5,13 +5,14 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Occupation(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner_occupations')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner_occupations' )
     client = models.ForeignKey(User, on_delete=models.CASCADE, related_name='client_occupations')
     occupation_type = models.CharField(max_length=20)
     annonce = models.ForeignKey(Annonce, on_delete=models.CASCADE, related_name='occupations')
     start_date = models.DateField(null=True, blank=True)
     end_date = models.DateField(null=True, blank=True)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
+    OfferedPrice = models.TextField(max_length=300,default="no price offered")
 
     def __str__(self):
         return f"{self.client.username} - {self.occupation_type}"
