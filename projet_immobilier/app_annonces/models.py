595d9d19cb08e2a4665bc9d07dbc4b032e7e5d9f
kacem <kacemmeherzi52@gmail.com>
2024-11-29 21:45:13 +0100

    improved is_occupied changed to boolean

--- projet_immobilier/app_annonces/models.py
+++ projet_immobilier/app_annonces/models.py
@@ -14,11 +14,11 @@ class Annonce(models.Model):
     description = models.TextField()
     nombre_de_chambres = models.IntegerField()
     surface = models.DecimalField(max_digits=10, decimal_places=2)
-    image  =  models.CharField(max_length=300,default="no image added")
+    image  =  models.CharField(max_length=1000,default="no image added")
     equiped = models.BooleanField(default=False)
     lease_duration = models.IntegerField(null=True, blank=True)  # Relevant for renting
     is_negotiable = models.BooleanField(default=False)  # Relevant for selling
-    is_occupied = models.CharField(max_length= 10 , default="no")
+    is_occupied = models.BooleanField(default=False)
     owner = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
 
 
