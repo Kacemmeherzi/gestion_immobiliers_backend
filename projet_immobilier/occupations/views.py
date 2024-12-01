46593911b9634946237ce3907e916625446c5beb
kacem <kacemmeherzi52@gmail.com>
2024-12-01 16:24:32 +0100

    status added

--- projet_immobilier/occupations/views.py
+++ projet_immobilier/occupations/views.py
@@ -62,11 +62,11 @@ def get_all_occupations(request):
         return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
     
 @api_view(['POST']) 
-def accept_occupation (request,id) : 
+def accept_occupation (request,id , status) : 
     try :
         occupation = Occupation.objects.get(pk=id)
 
-        occupation.is_active  = True 
+        occupation.is_active  = status
         occupation.save()
         annonce = occupation.annonce 
         annonce.is_occupied = True 
