1a27860380a1091c58e6a89b6b161302c3fc68b0
kacem <kacemmeherzi52@gmail.com>
2024-12-05 11:11:51 +0100

    swagger schema_view added

--- projet_immobilier/projet_immobilier/urls.py
+++ projet_immobilier/projet_immobilier/urls.py
@@ -18,6 +18,20 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your API",
+        default_version='v1',
+        description="API documentation",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="your_email@example.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('annonce/', include('app_annonces.urls')),
@@ -26,6 +40,7 @@ urlpatterns = [
     path('auth/', include('users.urls')),
     path('occupations/', include('occupations.urls')),
     path('predict/', include('prediction.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-schema'),
 
 
 
@@ -33,3 +48,4 @@ urlpatterns = [
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
